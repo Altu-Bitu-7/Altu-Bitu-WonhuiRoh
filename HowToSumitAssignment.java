beccea3077bf343a8b8ad546124a0f2007d52467
Wonhui Roh <rwhui012@gmail.com>
2024-08-13 20:57:42 +0900

    과제 제출 방법

--- HowToSumitAssignment.java
+++ HowToSumitAssignment.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class HowToSumitAssignment {
-}
+/*
+1. 알고리즘마다 branch 를 파서 코드를 업로드 합니다.
+2. 해당 branch -> main 으로 Pull request 를 작성합니다.
+    - Pull request 의 제목: '[알고리즘] 제출날짜'
+3. Pull request 의 링크를 안내된 구글폼으로 제출합니다.
+4. 튜터의 코드 리뷰 & 그 리뷰의 반영이 모두 끝난 후, 해당 Pull request 를 마저 merge 합니다.
+ */
