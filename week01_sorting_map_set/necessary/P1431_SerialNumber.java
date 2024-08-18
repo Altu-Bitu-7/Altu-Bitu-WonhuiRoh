54d092ff9901eedd85f113c9192f2efb46ca7020
Wonhui Roh <rwhui012@gmail.com>
2024-08-18 14:29:55 +0900

    [정렬] 8월 18일
    개선사항
    - o1 > o2 인 경우, o1 < o2 인 경우를 하나하나 분기 처리해줬던 것을, 하나의 분기에서 뺄셈 연산을 통해 양수 또는 음수의 결과가 나오게끔 개선함
    - o1.length() == o2.length() 인 경우에는 문자열들에 다시 조작을 가해 '문자열의 길이가 같다'는 공통점을 버려버리는 것이 아닌, 그 공통점을 살려 풀이하는 방식으로 개선함
    - 숫자인지 판별하는 코드를 isNumeric() 함수로 분리함

--- week01_sorting_map_set/necessary/P1431_SerialNumber.java
+++ week01_sorting_map_set/necessary/P1431_SerialNumber.java
@@ -6,6 +6,11 @@ import java.util.Scanner;
 public class P1431_SerialNumber {
     public static int N;
 
+    public static boolean isNumeric(char c) {
+        int num = c - '0';
+        return num > 0 && num < 10;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -18,27 +23,24 @@ public class P1431_SerialNumber {
             serialNumbers[i] = sc.next();
 
         Arrays.sort(serialNumbers, (o1, o2) -> {
-            int sumO1 = 0, sumO2 = 0;
+            if (o1.length() != o2.length())
+                return o1.length() - o2.length();
+            else { // o1.length() == o2.length()
+                int sum1 = 0, sum2 = 0;
 
-            if (o1.length() < o2.length())
-                return -1;
-            else if (o1.length() > o2.length())
-                return 1;
-            else {
-                String intStrO1 = o1.replaceAll("[^0-9]", "");
-                String intStrO2 = o2.replaceAll("[^0-9]", "");
+                for (int i = 0; i < o1.length(); i++) {
+                    char c1 = o1.charAt(i), c2 = o2.charAt(i);
 
-                for (int i = 0; i < intStrO1.length(); i++)
-                    sumO1 += Character.getNumericValue(intStrO1.charAt(i));
-                for (int i = 0; i < intStrO2.length(); i++)
-                    sumO2 += Character.getNumericValue(intStrO2.charAt(i));
+                    if (isNumeric(c1))
+                        sum1 += Character.getNumericValue(c1);
+                    if (isNumeric(c2))
+                        sum2 += Character.getNumericValue(c2);
+                }
 
-                if (sumO1 < sumO2)
-                    return -1;
-                else if (sumO1 > sumO2)
-                    return 1;
-                else
-                    return o1.compareTo(o2);
+                if (sum1 != sum2)
+                    return sum1 - sum2;
+
+                return o1.compareTo(o2);
             }
         });
 
